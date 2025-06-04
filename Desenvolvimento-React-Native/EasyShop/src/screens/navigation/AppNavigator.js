bce38b87a0fe7d7b702db62d80e62a6ec40f8159
Gustavo674 <gustavo.gouveia@sou.inteli.edu.br>
2025-06-04 11:36:43 -0300

    Create AppNavigator.js

---
+++ Desenvolvimento-React-Native/EasyShop/src/screens/navigation/AppNavigator.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import LoginScreen from '../../../src/screens/LoginScreen';
+import RegisterScreen from '../../../src/screens/RegisterScreen';
+import ResetPasswordScreen from '../../../src/screens/ResetPasswordScreen';
+import HomeScreen from '../../../src/screens/HomeScreen';
+import ProductDetailScreen from '../../../src/screens/ProductDetailScreen';
+import CameraScreen from '../../../src/screens/CameraScreen';
+import ProfileScreen from '../../../src/screens/ProfileScreen';
+import NotificationsScreen from '../../../src/screens/NotificationsScreen';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MainTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Camera" component={CameraScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Notifications" component={NotificationsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function AppNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
+      <Stack.Screen name="MainTabs" component={MainTabs} />
+      <Stack.Screen name="ProductDetail" component={ProductDetailScreen} />
+    </Stack.Navigator>
+  );
+}
